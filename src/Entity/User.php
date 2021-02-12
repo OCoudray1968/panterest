28682f39149530c56331162bd50b820760f66879
Olivier Coudray <ocoudray@hotmail.fr>
2021-02-12 15:53:51 +0100

    Display pin owner and getFullName method for user's name generation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -194,5 +194,8 @@ class User implements UserInterface
         return $this;
     }
 
-    
+     public function getFullName(): ?string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
 }
