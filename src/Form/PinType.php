0d2728f1c983db505d1a0847e098968087735a77
Olivier Coudray <ocoudray@hotmail.fr>
2021-02-19 08:08:16 +0100

    explicit is better than implicit

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -7,23 +7,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            
             ->add('imageFile', VichImageType::class, [
                 'label' => 'Image (JPG or NPG file)',
-                'required' => false,
                 'allow_delete' => true,
                 'download_uri' => false,
                 'imagine_pattern' => 'squared_thumbnail_small'
                 
             ])
-            
-            ->add('description')
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
             ;
     }
 
